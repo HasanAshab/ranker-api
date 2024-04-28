1a63bbbc71da4cb4fcf1c3e121bc9443919c9875
Your Name <you@example.com>
2024-04-28 14:10:56 +0600

    backup

--- api/challenges/serializers.py
+++ api/challenges/serializers.py
@@ -12,3 +12,9 @@ class ChallengeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Challenge
         exclude = ("user",)
+
+
+class ChallengeActivitiesSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    count = serializers.IntegerField()
--- api/challenges/views.py
+++ api/challenges/views.py
@@ -1,16 +1,19 @@
+from django.db.models import F, Count
 from rest_framework.permissions import (
     IsAuthenticated,
 )
-from rest_framework.views import APIView
 from rest_framework.generics import (
     ListAPIView,
     ListCreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
-from rest_framework.response import Response
 from .models import Challenge
 from .filters import ChallengeFilter
-from .serializers import ListChallengeSerializer, ChallengeSerializer
+from .serializers import (
+    ListChallengeSerializer,
+    ChallengeSerializer,
+    ChallengeActivitiesSerializer,
+)
 from .pagination import ChallengeCursorPagination
 
 
@@ -42,16 +45,6 @@ class ChallengeView(RetrieveUpdateDestroyAPIView):
         )
 
 
-from django.db.models import F, Count
-
-from rest_framework import serializers
-
-class ChallengeActivitiesSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    name = serializers.CharField()
-    count = serializers.IntegerField()
-
-
 class ChallengeActivitiesView(ListAPIView):
     serializer_class = ChallengeActivitiesSerializer
     queryset = Challenge.objects.none()
@@ -63,6 +56,6 @@ class ChallengeActivitiesView(ListAPIView):
             .annotate(
                 id=F("difficulty__id"),
                 name=F("difficulty__name"),
-                count=Count("id")
+                count=Count("id"),
             )
         )
