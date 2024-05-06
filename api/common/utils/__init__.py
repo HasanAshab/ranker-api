db130066fbd357f42eb5dec4affdb8dbb4e21595
Your Name <you@example.com>
2024-05-06 14:08:20 +0600

    backup

--- api/common/utils/__init__.py
+++ api/common/utils/__init__.py
@@ -1,12 +1,10 @@
 from .mail import send_mail
 from .proxy import LazyProxy
-from .env import env_file
 from .twilio import twilio_verification
 
 
 __all__ = [
     "send_mail",
     "LazyProxy",
-    "env_file",
     "twilio_verification",
 ]
