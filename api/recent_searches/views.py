71c3a785807f949873fd57136659d15a31c5d4d4
Your Name <you@example.com>
2024-05-07 22:17:01 +0600

    backup

--- api/recent_searches/views.py
+++ api/recent_searches/views.py
@@ -1,37 +1,17 @@
-from rest_framework.generics import ListAPIView, DestroyAPIView
+from rest_framework.generics import DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from api.common.generics import ListBulkDestroyAPIView
 from .models import RecentUserSearch
 from .serializers import ListRecentUserSearchSerializer
 from .pagination import RecentUserSearchPagination
 
 
-from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
-
-
-class BulkDestroyAPIView(GenericAPIView):
-    def perform_destroy(self, queryset):
-        queryset.delete()
-
-    def delete(self, request):
-        self.perform_destroy(self.get_queryset())
-        return Response(status=204)
-
-
-class ListDestroyAPIView(ListAPIView):
-    def perform_destroy(self, instance):
-        instance.delete()
-
-    def delete(self, request):
-        for insance in self.get_queryset():
-            self.perform_destroy(insance)
-        return Response(status=204)
-
-
-class ListBulkDestroyAPIView(ListAPIView, BulkDestroyAPIView):
-    pass
-
-
+@extend_schema_view(
+    delete=extend_schema(
+        operation_id="recent_searches_destroy_all",
+    ),
+)
 class RecentUserSearchesView(ListBulkDestroyAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = RecentUserSearch.objects.none()
