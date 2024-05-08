f4ecfa3633062e8a96654d8526c334f89a3296f9
Your Name <you@example.com>
2024-05-08 15:32:52 +0600

    backup

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from allauth.headless.constants import Client
-from allauth.headless.account.views import ManageEmailView
 from .views import (
     UsersView,
     ProfileView,
@@ -9,8 +7,6 @@ from .views import (
     PhoneNumberView,
 )
 
-
-client = Client.APP
 
 urlpatterns = [
     path(
@@ -22,11 +18,6 @@ urlpatterns = [
         "users/me/",
         ProfileView.as_view(),
         name="profile",
-    ),
-    path(
-        "users/me/email/",
-        ManageEmailView.as_api_view(client=client),
-        name="manage_email",
     ),
     path(
         "users/me/phone-number/",
