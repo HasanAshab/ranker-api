a1258b0a2533197956db1a8bf953dbc649384640
Your Name <you@example.com>
2024-05-24 12:50:34 +0600

    backup

--- api/challenges/factories.py
+++ api/challenges/factories.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from django.utils import timezone
 import factory
-from .models import Challenge
+from .models import Challenge, ChallengeStep
 
 
 class ChallengeFactory(factory.django.DjangoModelFactory):
@@ -21,3 +21,11 @@ class ChallengeFactory(factory.django.DjangoModelFactory):
         has_due_date = factory.Trait(
             due_date=timezone.now() + timedelta(days=1)
         )
+
+
+class ChallengeStepFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker("sentence", nb_words=3)
+    challenge = factory.SubFactory("api.challenges.factories.ChallengeFactory")
+
+    class Meta:
+        model = ChallengeStep
