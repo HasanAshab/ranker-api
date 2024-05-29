aa4de567890cc9de8ca9c9c584202904d74b0286
Your Name <you@example.com>
2024-05-29 12:09:05 +0600

    backup

--- api/users/admin.py
+++ api/users/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import UserModel
+from django.contrib.auth import get_user_model
 
-
-admin.site.register(UserModel)
+admin.site.register(get_user_model())
