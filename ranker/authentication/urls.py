d5d0d36d19d686d3650da26087c53e76c10f73fc
Your Name <you@example.com>
2024-08-03 11:40:19 +0600

    backup

--- ranker/authentication/urls.py
+++ ranker/authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import TokenSSEView, TokenLoginView
+from .views import LoginTokenSSEView, TokenLoginView
 
 urlpatterns = [
     path("_allauth/", include("allauth.headless.urls")),
-    path("auth/token/sse/", TokenSSEView.as_view(), name="token_sse"),
+    path("auth/token/sse/", LoginTokenSSEView.as_view(), name="token_sse"),
     path("auth/token/login/", TokenLoginView.as_view(), name="token_login"),
 ]
--- ranker/authentication/views.py
+++ ranker/authentication/views.py
@@ -16,13 +16,12 @@ AuthToken = get_token_model()
 login_token_signer = TimestampSigner(salt=settings.TOKEN_LOGIN_SALT)
 
 
-class TokenSSEView(APIView):
+class LoginTokenSSEView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def token_generator(self):
         while True:
             token = login_token_signer.sign(self.request.user.username)
-            print(token)
             yield token
             time.sleep(settings.TOKEN_LOGIN_MAX_AGE)
 
@@ -44,5 +43,4 @@ class TokenLoginView(APIView):
         )
         user = User.objects.get(username=username)
         _, api_token = AuthToken.objects.create(user)
-        print(user)
         return Response(api_token)
