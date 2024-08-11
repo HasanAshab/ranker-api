53478960c42b64b72686b3764ca68590195cb3c0
Your Name <you@example.com>
2024-08-11 11:09:43 +0600

    backup

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,17 +1,18 @@
 from .base import *
-from .base import env
+from .base import env, BASE_DIR
 
 
 DEBUG = False
 
 ALLOWED_HOSTS = ["ranker.pythonanywhere.com"]
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
-        "LOCATION": "127.0.0.1:11211",
-    }
-}
+# CACHE NOT REQUIRED FOR NOW
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+#         "LOCATION": "127.0.0.1:11211",
+#     }
+# }
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "localhost"
@@ -23,13 +24,23 @@ EMAIL_USE_TLS = True
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+# GOT NO FREE SERVICES
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "HOST": env("DB_HOST"),
+#         "PORT": env("DB_PORT"),
+#         "NAME": env("DB_NAME"),
+#         "USER": env("DB_USER"),
+#         "PASSWORD": env("DB_PASSWORD"),
+#     },
+# }
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": env("DB_HOST"),
-        "PORT": env("DB_PORT"),
-        "NAME": env("DB_NAME"),
-        "USER": env("DB_USER"),
-        "PASSWORD": env("DB_PASSWORD"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     },
 }
