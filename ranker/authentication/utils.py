6584fbc43dc5b5aeb6204352cea4fbe9e0f20a29
HasanAshab <samertask@gmail.com>
2024-08-12 10:00:01 +0600

    backup

--- ranker/authentication/utils.py
+++ ranker/authentication/utils.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.core.signing import TimestampSigner
 from knox.models import get_token_model
 from ranker.users.models import User
@@ -9,12 +10,15 @@ login_token_signer = TimestampSigner(salt=settings.TOKEN_LOGIN_SALT)
 
 
 def generate_login_token(user):
-    return login_token_signer.sign(user.username)
+    signature = login_token_signer.sign(user.username)
+    token = urlsafe_base64_encode(signature)
+    return token
 
 
 def login_using_token(token):
+    signature = urlsafe_base64_decode(token)
     username = login_token_signer.unsign(
-        token, max_age=settings.TOKEN_LOGIN_MAX_AGE
+        signature, max_age=settings.TOKEN_LOGIN_MAX_AGE
     )
     user = User.objects.get(username=username)
     _, api_token = AuthToken.objects.create(user)
