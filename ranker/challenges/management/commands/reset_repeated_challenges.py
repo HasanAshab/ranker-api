36e8e732da3188d14ffb8ff6411b90e7e87d47ee
Your Name <you@example.com>
2024-08-25 11:41:42 +0600

    backup

--- ranker/challenges/management/commands/reset_repeated_challenges.py
+++ ranker/challenges/management/commands/reset_repeated_challenges.py
@@ -29,7 +29,6 @@ class Command(BaseCommand):
         )
 
     def handle(self, repeat_type, **kwargs):
-        self.seed()
         daily_challenges = (
             Challenge.objects.active()
             .repeated(repeat_type)
@@ -54,48 +53,12 @@ class Command(BaseCommand):
                 challenge.mark_as_active(commit=False)
 
             with transaction.atomic():
-                print(user_chunk)
                 Challenge.objects.bulk_update(challenge_chunk, ["status"])
                 User.objects.bulk_update(user_chunk, ["total_xp"])
-
         Challenge.objects.repeated(repeat_type).inactive().mark_as_active()
-        self.stdout.write("Successfully reset daily challenges of all users")
 
-        self.user1.refresh_from_db()
-        self.user2.refresh_from_db()
-        print(self.user1, self.user1.total_xp)
-        print(self.user2, self.user2.total_xp)
-
-        print(self.user2.challenge_set.completed().count())
-
-    def seed(self):
-        from ranker.difficulties.models import Difficulty
-        from ranker.users.factories import UserFactory
-        from ranker.challenges.factories import ChallengeFactory
-
-        Challenge.objects.all().delete()
-        User.objects.all().delete()
-
-        diff = Difficulty.objects.first()
-        print(diff, diff.xp_value)
-
-        self.user1 = UserFactory(username="hasan", total_xp=50)
-        self.user2 = UserFactory(username="hossein", total_xp=100)
-
-        ChallengeFactory(
-            user=self.user2,
-            repeat_type=Challenge.RepeatType.DAILY,
-            difficulty=diff,
-        )
-        ChallengeFactory.create_batch(
-            2,
-            user=self.user1,
-            repeat_type=Challenge.RepeatType.DAILY,
-            difficulty=diff,
-        )
-        ChallengeFactory(
-            user=self.user2,
-            repeat_type=Challenge.RepeatType.DAILY,
-            difficulty=diff,
-            completed=True,
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Successfully reset daily challenges of all users"
+            )
         )
