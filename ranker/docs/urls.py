d0855c4694cbeaae38e7aab1cbef24b795aca1ee
Your Name <you@example.com>
2024-09-02 11:21:20 +0600

    backup

--- ranker/docs/urls.py
+++ ranker/docs/urls.py
@@ -1,13 +1,14 @@
+from django.conf import settings
 from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from .views import SchemaView
 
 
 urlpatterns = [
-    path("docs/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "docs/ui/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
@@ -19,3 +20,13 @@ urlpatterns = [
         name="redoc",
     ),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [
+        path("docs/schema/", SpectacularAPIView.as_view(), name="schema")
+    ]
+else:
+    urlpatterns += [
+        path("docs/schema/", SchemaView.as_view(), name="schema"),
+    ]
---
+++ ranker/docs/views.py
@@ -0,0 +1,12 @@
+import os
+from django.http import FileResponse
+from django.conf import settings
+from rest_framework.views import APIView
+
+
+class SchemaView(APIView):
+    def get(self, request, *args, **kwargs):
+        schema_path = os.path.join(settings.STATIC_ROOT, "docs/schema.yml")
+        return FileResponse(
+            open(schema_path, "rb"), content_type="application/x-yaml"
+        )
