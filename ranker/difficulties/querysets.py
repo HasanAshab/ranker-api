2665250ad8a221b4d0d6d453493c23865ecebde4
Your Name <you@example.com>
2024-10-05 11:58:34 +0600

    backup

--- ranker/difficulties/querysets.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class DifficultyQuerySet(models.QuerySet):
-    def with_challenge_count(self):
-        return self.annotate(
-            challenge_count=models.Count("challenge", distinct=True)
-        )
